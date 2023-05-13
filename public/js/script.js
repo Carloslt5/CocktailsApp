5e54bb89d9c178b7fe6271cdcc3b06c038f77884
Carlos Liao <93123435+Carloslt5@users.noreply.github.com>
2023-05-13 18:31:18 +0200

    init

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("cocktailsApp JS imported successfully!");
+});
