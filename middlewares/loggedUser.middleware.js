b6461318d4040bf3cefb6f3998e4191b30a59e46
Carloslt <carloslt.proyectos@gmail.com>
2023-05-18 16:11:04 +0200

    fixed

--- middlewares/loggedUser.middleware.js
+++ middlewares/loggedUser.middleware.js
@@ -1,5 +1,6 @@
 const updateLoggedUser = (req, res, next) => {
     res.locals.loggedUser = req.session.currentUser
+    res.locals.userIsAdmin = req.session.currentUser?.role === 'ADMIN'
     next()
 }
 
